60d331cdd7c00ba7c66a02f0920225c7b3487216
thi <thinguyen01683@gmail.com>
2023-12-06 07:47:29 +0700

    day 2 2.1 connect mongodb

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const app = require("./app");
+const mongoose = require('mongoose');
 require("dotenv").config();
 
 const port = process.env.PORT || 4001;
-app.listen(port, () => {
-  console.log(`server running at ${port}`);
-});
+mongoose.connect(process.env.MONGO_URI).then(conn => {
+  console.log(`server connected`);
+  app.listen(port, () => {
+    console.log(`server running at ${port}`);
+  });
+  
+}).catch(err => {
+  console.log(`server disconnected`);
+})
